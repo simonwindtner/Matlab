df0012c8ba0af4c4079caa4ac3f642f94ddaef46
Simon Windtner <simon.windtner@gmail.com>
2020-02-17 17:06:45 +0100

    first release

--- SP_Source_Roomsimulation_directivity.m
+++ SP_Source_Roomsimulation_directivity.m
@@ -1,8 +1,11 @@
-%%--------------------------------------------------------------------------
+%%-------------------------------------------------------------------------
+% Windtner Simon 2020-02
+% release 1.0
 % short script to simulate sound pressure level trend 
 % for given amount of microphones in a room 
 % sources are simulated for each x,y and z coordinate in the room 
-% Test
+% consideration of cardioid characteristics of microphone 
+% consideration of direction of microphones
 %--------------------------------------------------------------------------
 
 clear all;
@@ -11,70 +14,104 @@ clc;
 
 %% define number of sources and mic positions
 % mic position between 0..1 for x,y and z coordinates 
-% all vectors musst be the same size
-mic_x = [.2, .5, .8];
-mic_y = [.5, .2, .5];
-mic_z = [.5, .5, .5];
+% all vectors musst be the same size!
+
+mic_x = [.2, .4, .6, .8];
+mic_y = [.5, .2, .2, .5];
+mic_z = [.5, .5, .5, .5];
+
 % mic_direction is seen like this
-% - ----> 0°C <---- +
-mic_direction = [45, 0, -45];
+% Y
+% 1   --------------------------------
+%    |     + ----> 0°C <---- -        |
+%    |                                |
+%    |         \   |   /              |
+%    |          \  |  /               |
+%    |             x                  |
+%    |                                |
+% 0   --------------------------------
+mic_direction = [30, 0, 0, -30];
 
-
+% roomscale -> size of room (standard: 1x1x1)
 roomscale = 1;
+% gridscale -> points of meshgrid resolutions
 gridscale = 0.01;
-Z_cs = 1 * 100;
 
-arrow_x = [.2 .2; .45 .45; .8 .8];
-arrow_y = [.5 .8; .2 .5; .5 .8];
+Z_level = 1 / gridscale;
 
 
 %% meshgrid to simulate source positions
-%  radius will be referenced to the mid radius of the room r(0.5,0.5,0.5)
 %  SPL is simulated for each microphone seperatley 
+
+% preallocation of meshgrid and vectors
 [X,Y,Z] = meshgrid(0:gridscale:1 * roomscale);
 SPL = X;
-n_db = X;
-niere = X;
+SPL_cardioid_char = X;
+SPL_cardioid_char_total = X;
+alpha = X;
 SPL(:,:,:) = -100;
-n_db(:,:,:) = -100;
-niere(:,:,:) =  -100;
-for Z_cs = 1:10:Z_cs 
+SPL_cardioid_char(:,:,:) = -100;
+SPL_cardioid_char_total(:,:,:) =  -100;
+
+
+for Z_level = 1:1:Z_level 
     for n = 1:length(mic_x)
+        % calculation of distance between microphone and simulated source
+        % position -> radius
         dx = X - mic_x(n);  
         dy = Y - mic_y(n);
         dz = Z - mic_z(n);
-        %dx_rot = dx .* cosd(mic_direction(n)) - dy .* sind(mic_direction(n));
-        %dy_rot = dx .* sind(mic_direction(n)) - dy .* cosd(mic_direction(n));
         r_source = sqrt(dx.^2 + dy.^2 + dz.^2) + 1;
-        alpha = acosd(dy ./ sqrt(dx.^2 + dy.^2) );%+ mic_direction(n);
+        % calculation of angle between microphone and source position
         for col = 1:length(X)
             for row = 1:length(X)
-                n_db(row,col,Z_cs) = 20*log10((1 + cosd(alpha(row,col,Z_cs)))/2);
+                if dx(row,col,Z_level) < 0 
+                    alpha(row,col,Z_level) = 360 - acosd(dy(row,col,Z_level) / sqrt(dx(row,col,Z_level)^2 + dy(row,col,Z_level)^2));
+                else 
+                    alpha(row,col,Z_level) =  acosd(dy(row,col,Z_level) / sqrt(dx(row,col,Z_level)^2 + dy(row,col,Z_level)^2));
+                end    
             end
         end
-        niere = max(n_db,niere);
-        %surf(X(:,:,Z_cs),Y(:,:,Z_cs),niere(:,:,Z_cs));
-        S = -20*log10(r_source);
-        % / (sqrt(3)/2)
-        SPL = max(SPL,S);
-        SPL_neu = SPL + niere;
-    end    
-    temp = figure(Z_cs)
-    surf(X(:,:,Z_cs),Y(:,:,Z_cs),SPL_neu(:,:,Z_cs));
+        
+        % consideration of microphone direction
+        alpha = wrapTo360(alpha + mic_direction(n));
+        
+        % calculation of cardioid characterisation
+        for col = 1:length(X)
+            for row = 1:length(X)
+                if alpha(row,col,Z_level) == 360.0
+                    alpha(row,col,Z_level) = 0;
+                end    
+                SPL_cardioid_char(row,col,Z_level) = 20*log10((1 + cosd(alpha(row,col,Z_level)))/2);
+            end
+        end
+        
+        SPL_cardioid_char_total = max(SPL_cardioid_char,SPL_cardioid_char_total);
+        SPL_radius = -20*log10(r_source);
+        SPL = max(SPL,SPL_radius);
+        SPL_total = SPL + SPL_cardioid_char_total;
+    end
+    % surfaceplot of SPL over X,Y for current Z - level
+    temp = figure(Z_level)
+    surf(X(:,:,Z_level),Y(:,:,Z_level),SPL_total(:,:,Z_level));
     xlabel('X')
     ylabel('Y')
+    shading interp
     caxis([-10 0])
     view(2)
     colorbar
+    
+    % plot of microphone position and arrows for direction
     for n = 1:length(mic_x)
         hold on 
         plot(mic_x(n),mic_y(n) ,'r*')
-        arrow = annotation('arrow',arrow_x(n,1:end), arrow_y(n,1:end));
-        arrow.Color = 'red';
-        arrow.LineWidth = 0.85; 
+        p0 = [mic_x(n) mic_y(n) 0.0];
+        p1 = [(mic_x(n) - tand(mic_direction(n) * 0.25)) (mic_y(n) + .25) 0.0];
+        vectarrow(p0,p1);
     end
-% for exporting the plots storage path has to be changed
-     title(['SPL trend for Z - level ' num2str(Z_cs)])
+    
+% for exporting the plots storage path has to be adapted
+     title(['SPL trend for Z - level ' num2str(Z_level * gridscale)])
 %     print(temp, '-djpeg' , ['/Users/simonwindtner/Documents/ET-TI/SMA/Ensemble/'...
 %                      'Z' num2str(Z_cs)]);
 end
